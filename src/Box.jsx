ef576bc705aad1c32e824c7b264c33a9820d37a9
Vladislav Kotsev <123163924+SamuilNikolov00@users.noreply.github.com>
2023-01-31 23:08:17 +0200

    Revert "Revert "Game finished""
    This reverts commit 639c6d0e9e8bb59d1ce3620c452a4ffe61293876.

--- src/Box.jsx
+++ src/Box.jsx
@@ -3,15 +3,23 @@ import React from "react";
 export default function Box(props) {
   
 
-  const color = { backgroundColor: props.on ? "white" : "black" };
+  let className;
+  if(props.victory){
+    className = "win-box";
+  }else if(props.locked){
+    className = "box locked";
+  }else if(!props.locked){
+    className = "box";
+  }
+ 
 
   return (
     <>
       <div
-        className="box"
-        style={color}
-        onClick={props.clickHandler}
-      ></div>
+        className={className}
+        
+        onClick={()=>props.clickHandler(props.id)}
+      >{props.value}</div>
     </>
   );
 }
